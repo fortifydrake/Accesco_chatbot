7876fb6c38b49e0d1b4d80cf74100dc97e04dcc7
Fortify Drake <prasadaditya316@gmail.com>
2025-12-14 12:51:31 +0530

    db

--- app/config.py
+++ app/config.py
@@ -1,3 +1,7 @@
 import os
 
-DATABASE_URL = "postgresql://postgres:@Aditya123@localhost/accesco_bot"
+DATABASE_URL = os.getenv("postgresql://accesco_chatbot_user:VkYQHnBDYuynrfoCVIOCNuf0w83nnkCs@dpg-d4v66deuk2gs7397bj2g-a/accesco_chatbot")
+
+from sqlalchemy import create_engine
+
+engine = create_engine("postgresql://accesco_chatbot_user:VkYQHnBDYuynrfoCVIOCNuf0w83nnkCs@dpg-d4v66deuk2gs7397bj2g-a/accesco_chatbot")
