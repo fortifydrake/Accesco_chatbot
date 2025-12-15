7d71dc6eb0e4f909de022c627b0803d116babbf0
Fortify Drake <prasadaditya316@gmail.com>
2025-12-15 11:27:33 +0530

    testing db

--- app/config.py
+++ app/config.py
@@ -1,3 +1,14 @@
 import os
 
-DATABASE_URL = "postgresql://postgres:@Aditya123@localhost/accesco_bot"
+class Settings:
+    # App info
+    APP_NAME = "Accesco Chatbot"
+    ENV = os.getenv("ENV", "development")
+
+    # Database (Supabase / Render / Local)
+    DATABASE_URL = os.getenv("postgresql://postgres:@Adit779ya@db.slhrjrvlaukebpmgxipk.supabase.co:5432/postgres")
+
+    # Security (optional, future use)
+    SECRET_KEY = os.getenv("U7lkuEXEm8", "dev-secret")
+
+settings = Settings()
